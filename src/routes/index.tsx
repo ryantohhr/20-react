3823a670fe990122459e761debde570635144ea4
ryantohhr <ryantohhongrui@hotmail.com>
2025-06-13 21:53:54 +0800

    Initialised index.tsx to generate challenge links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,25 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+    const challenges = [
+        {
+            title: 'Rock Paper Scissors',
+            path: '/rock-paper-scissors',
+        }
+    ]
+  return (
+    challenges.map((challenge) => {
+        return (
+            <div>
+                <Link to={challenge.path}>
+                    {challenge.title}
+                </Link>
+            </div>
+        )
+    })
+  )
 }
