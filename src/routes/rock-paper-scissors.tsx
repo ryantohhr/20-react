0e9ef5dc7f5e2e9d0a1624689f8631d28149d610
ryantohhr <ryantohhongrui@hotmail.com>
2025-06-13 22:09:18 +0800

    Created weapon options screen

--- src/routes/rock-paper-scissors.tsx
+++ src/routes/rock-paper-scissors.tsx
@@ -5,5 +5,14 @@ export const Route = createFileRoute('/rock-paper-scissors')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/rock-paper-scissors"!</div>
+  return (
+    <div className='flex flex-col items-center justify-center h-screen gap-5'>
+        <p className='text-2xl font-bold'>Pick your weapon</p>
+        <div className='flex items-center justify-center gap-3'>
+            <button className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Rock</button>
+            <button className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Paper</button>
+            <button className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Scissors</button>
+        </div>
+    </div>
+)
 }
