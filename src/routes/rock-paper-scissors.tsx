10ac37e430111c8a33800f26629e9d252f146d13
ryantohhr <ryantohhongrui@hotmail.com>
2025-06-14 00:02:55 +0800

    Implemented base game logic

--- src/routes/rock-paper-scissors.tsx
+++ src/routes/rock-paper-scissors.tsx
@@ -1,18 +1,86 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { setgid } from 'process'
+import { useState, useEffect } from 'react'
 
 export const Route = createFileRoute('/rock-paper-scissors')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const [userChoice, setUserChoice] = useState<string | null>(null)
+  const [computerChoice, setComputerChoice] = useState<string | null>(null)
+  const [gameState, setGameState] = useState<string>('playing')
+
+  useEffect(() => {
+    determineWinner()
+  }, [userChoice, computerChoice])
+
+  function chooseRandom() {
+    const choices = ['rock', 'paper', 'scissors']
+    setComputerChoice(choices[Math.floor(Math.random() * 3)])
+  }
+
+  function chooseRock() {
+    setUserChoice('rock')
+    chooseRandom()
+  }
+
+  function choosePaper() {
+    setUserChoice('paper')
+    chooseRandom()
+  }
+
+  function chooseScissors() {
+    setUserChoice('scissors')
+    chooseRandom()
+  }
+
+  function determineWinner() {
+    if (!userChoice || !computerChoice) {
+      setGameState('playing')
+    } else if (userChoice === computerChoice) {
+      setGameState('tie')
+    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
+      setGameState('win')
+    } else if (userChoice === 'paper' && computerChoice === 'rock') {
+      setGameState('win')
+    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
+      setGameState('win')
+    } else {
+      setGameState('lose')
+    }
+  }
+
+  function displayResult() {
+    if (gameState === 'tie') {
+      return <div>It's a tie!</div>
+    } else if (gameState === 'win') {
+      return <div>You won!</div>
+    } else if (gameState === 'lose') {
+      return <div>You lose!</div>
+    }
+  }
+
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-5'>
-        <p className='text-2xl font-bold'>Pick your weapon</p>
-        <div className='flex items-center justify-center gap-3'>
-            <button className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Rock</button>
-            <button className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Paper</button>
-            <button className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Scissors</button>
-        </div>
+      {gameState === 'playing' ? (
+          <>
+            <p className='text-2xl font-bold'>Pick your weapon</p>
+              <div className='flex items-center justify-center gap-3'>
+                  <button onClick={chooseRock} className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Rock</button>
+                  <button onClick={choosePaper} className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Paper</button>
+                  <button onClick={chooseScissors} className='w-44 h-80 border-2 border-white rounded-md bg-[#120d33]'>Scissors</button>
+              </div>
+            
+          </>
+        ) : (
+          <>
+            {displayResult()}
+            You chose {userChoice}, CPU chose {computerChoice}
+          </>
+        )  
+      }
     </div>
+
 )
 }
