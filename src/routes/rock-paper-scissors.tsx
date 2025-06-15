a31dddf00ba452ba4180b420896c665098e0e192
ryantohhr <ryantohhongrui@hotmail.com>
2025-06-15 13:25:33 +0800

    Refactored final version
    1. Created weapon types
    2. Refactored individual chooseRock, choosePaper and choose Scissors to a single handleUserChoice function
    3. Reduced determineWinner and displayResult to a single function, removing useEffect

--- src/routes/rock-paper-scissors.tsx
+++ src/routes/rock-paper-scissors.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { setgid } from 'process'
 import { useState, useEffect } from 'react'
 import { BrickWall, ScrollText, Scissors, Trophy, Trash2, Equal } from 'lucide-react'
 
@@ -7,59 +6,37 @@ export const Route = createFileRoute('/rock-paper-scissors')({
   component: RouteComponent,
 })
 
+type Weapon = 'rock' | 'paper' | 'scissors'
+const weapons = ['rock', 'paper', 'scissors'] as const
+
 function RouteComponent() {
-  const [userChoice, setUserChoice] = useState<string | null>(null)
-  const [computerChoice, setComputerChoice] = useState<string | null>(null)
-  const [gameState, setGameState] = useState<string>('playing')
+  const [userChoice, setUserChoice] = useState<Weapon | null>(null)
+  const [computerChoice, setComputerChoice] = useState<Weapon | null>(null)
+  const [gameState, setGameState] = useState<'playing' | 'done'>('playing')
 
-  useEffect(() => {
-    determineWinner()
-  }, [userChoice, computerChoice])
-
-  function chooseRandom() {
-    const choices = ['rock', 'paper', 'scissors']
-    setComputerChoice(choices[Math.floor(Math.random() * 3)])
-  }
-
-  function chooseRock() {
-    setUserChoice('rock')
-    chooseRandom()
-  }
-
-  function choosePaper() {
-    setUserChoice('paper')
-    chooseRandom()
-  }
-
-  function chooseScissors() {
-    setUserChoice('scissors')
-    chooseRandom()
-  }
-
-  function determineWinner() {
-    if (!userChoice || !computerChoice) {
-      setGameState('playing')
-    } else if (userChoice === computerChoice) {
-      setGameState('tie')
-    } else if (userChoice === 'rock' && computerChoice === 'scissors') {
-      setGameState('win')
-    } else if (userChoice === 'paper' && computerChoice === 'rock') {
-      setGameState('win')
-    } else if (userChoice === 'scissors' && computerChoice === 'paper') {
-      setGameState('win')
-    } else {
-      setGameState('lose')
-    }
+  function handleUserChoice(weapon: Weapon) {
+    setUserChoice(weapon)
+    setComputerChoice(weapons[Math.floor(Math.random() * weapons.length)])
+    setGameState('done')
   }
 
   function displayResult() {
-    if (gameState === 'tie') {
-      return <div className='flex flex-col items-center justify-center gap-2 text-2xl font-bold'>It's a tie! <Equal className='w-10 h-10' /></div>
-    } else if (gameState === 'win') {
-      return <div className='flex flex-col items-center justify-center gap-2 text-center text-2xl font-bold'>You won! <br /> You chose {userChoice}, CPU chose {computerChoice} <Trophy className='w-10 h-10' /></div>
-    } else if (gameState === 'lose') {
-      return <div className='flex flex-col items-center justify-center gap-2 text-center text-2xl font-bold'>You lose! <br /> You chose {userChoice}, CPU chose {computerChoice} <Trash2 className='w-10 h-10' /></div>
+    let result
+    if (!userChoice || !computerChoice) {
+      setGameState('playing')
+    } else if (userChoice === computerChoice) {
+      result = <div className='flex flex-col items-center justify-center gap-2 text-2xl font-bold'>It's a tie! <Equal className='w-10 h-10' /></div>
+    } else if (
+      (userChoice === 'rock' && computerChoice === 'scissors') ||
+      (userChoice === 'paper' && computerChoice === 'rock') ||
+      (userChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      result = <div className='flex flex-col items-center justify-center gap-2 text-center text-2xl font-bold'>You won! <br /> You chose {userChoice}, CPU chose {computerChoice} <Trophy className='w-10 h-10' /></div>
+    } else {
+      result = <div className='flex flex-col items-center justify-center gap-2 text-center text-2xl font-bold'>You lose! <br /> You chose {userChoice}, CPU chose {computerChoice} <Trash2 className='w-10 h-10' /></div>
     }
+
+    return result
   }
 
   function resetGame() {
@@ -74,9 +51,18 @@ function RouteComponent() {
           <>
             <p className='text-2xl font-bold'>Pick your weapon</p>
               <div className='flex items-center justify-center gap-3'>
-                  <button onClick={chooseRock} className='w-44 h-80 flex flex-col items-center justify-center gap-2 border-2 border-white rounded-md bg-[#120d33]'><BrickWall className='w-10 h-10' />ROCK</button>
-                  <button onClick={choosePaper} className='w-44 h-80 flex flex-col items-center justify-center gap-2 border-2 border-white rounded-md bg-[#120d33]'><ScrollText className='w-10 h-10' />PAPER</button>
-                  <button onClick={chooseScissors} className='w-44 h-80 flex flex-col items-center justify-center gap-2 border-2 border-white rounded-md bg-[#120d33]'><Scissors className='w-10 h-10'/>SCISSORS</button>
+                  <button onClick={() => {
+                    handleUserChoice('rock')
+                  }}
+                  className='w-44 h-80 flex flex-col items-center justify-center gap-2 border-2 border-white rounded-md bg-[#120d33]'><BrickWall className='w-10 h-10' />ROCK</button>
+                  <button onClick={() => {
+                    handleUserChoice('paper')
+                  }}
+                  className='w-44 h-80 flex flex-col items-center justify-center gap-2 border-2 border-white rounded-md bg-[#120d33]'><ScrollText className='w-10 h-10' />PAPER</button>
+                  <button onClick={() => {
+                    handleUserChoice('scissors')
+                  }}
+                  className='w-44 h-80 flex flex-col items-center justify-center gap-2 border-2 border-white rounded-md bg-[#120d33]'><Scissors className='w-10 h-10'/>SCISSORS</button>
               </div>
             
           </>
