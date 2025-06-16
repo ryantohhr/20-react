565b1ccd3a914a8ec810750e473ffb0e62716211
ryantohhr <ryantohhongrui@hotmail.com>
2025-06-16 13:25:37 +0800

    Refactored final version
    1. Refactored single timeControl function to start and pause for readability and simplicity
    2. Changed interval ID state to use useRef for interval ID updating
    3. Refactored conditional rendering to only involve buttons
    4. Renamed stopwatch states for clarity

--- src/routes/stopwatch.tsx
+++ src/routes/stopwatch.tsx
@@ -1,65 +1,58 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 export const Route = createFileRoute('/stopwatch')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  const [secondsElapsed, setSecondsElapsed] = useState<number>(0)
-  const [stopwatchState, setStopwatchState] = useState<'start' | 'running' | 'stopped'>('start')
-  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(undefined)
+  const [secondsElapsed, setSecondsElapsed] = useState(0)
+  const [stopwatchState, setStopwatchState] = useState<'initial' | 'running' | 'paused'>('initial')
+  const intervalRef = useRef<NodeJS.Timeout | undefined>(undefined)
 
-  function intervalCallback(): void {
-    setSecondsElapsed((prev) => prev + 1)
+  function start() {
+    intervalRef.current = setInterval(() => {
+      setSecondsElapsed((prev) => prev + 1)
+    }, 1000)
+    setStopwatchState('running')
   }
 
-  function timeControl() {
-    if (stopwatchState === 'start' || stopwatchState === 'stopped') {
-      setIntervalId(setInterval(intervalCallback, 1000))
-      setStopwatchState('running')
-    } else if (stopwatchState === 'running') {
-      clearInterval(intervalId)
-      setStopwatchState('stopped')
-    }
+  function pause() {
+    clearInterval(intervalRef.current)
+    setStopwatchState('paused')
   }
 
-  function resetTime() {
+  function reset() {
     setSecondsElapsed(0)
-    setStopwatchState('start')
+    setStopwatchState('initial')
   }
 
   return (
     <div className='flex items-center justify-center h-screen'>
-      {stopwatchState === 'start' && (
-        <div className='w-72 h-56 flex flex-col items-center justify-evenly border border-white rounded-md'>
-          {secondsElapsed} seconds elapsed
-          <button onClick={timeControl} className='px-4 py-2 bg-green-500 text-white rounded-md'>
+      <div className='w-72 h-56 flex flex-col items-center justify-evenly border border-white rounded-md'>
+        {secondsElapsed} seconds elapsed
+        {stopwatchState === 'initial' && (
+          <button onClick={start} className='px-4 py-2 bg-green-500 text-white rounded-md'>
             Start
           </button>
-        </div>
-      )}
-      {stopwatchState === 'running' && (
-        <div className='w-72 h-56 flex flex-col items-center justify-evenly border border-white rounded-md'>
-          {secondsElapsed} seconds elapsed
-          <button onClick={timeControl} className='px-4 py-2 bg-gray-500 text-white rounded-md'>
-            Pause
-          </button>
-      </div>
-      )}
-      {stopwatchState === 'stopped' && (
-        <div className='w-72 h-56 flex flex-col items-center justify-evenly border border-white rounded-md'>
-          {secondsElapsed} seconds elapsed
+        )}
+
+        {stopwatchState === 'running' && (
+        <button onClick={pause} className='px-4 py-2 bg-gray-500 text-white rounded-md'>
+          Pause
+        </button>
+        )}
+        {stopwatchState === 'paused' && (
           <div className='flex items-center justify-center gap-4'>
-            <button onClick={timeControl} className='px-4 py-2 bg-gray-500 text-white rounded-md'>
+            <button onClick={start} className='px-4 py-2 bg-gray-500 text-white rounded-md'>
               Resume
             </button>
-            <button onClick={resetTime} className='px-4 py-2 bg-red-500 text-white rounded-md'>
+            <button onClick={reset} className='px-4 py-2 bg-red-500 text-white rounded-md'>
               Reset
             </button>
           </div>
+        )}
       </div>
-      )}
     </div>
   )
 }
