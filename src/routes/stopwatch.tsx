bfe271fd8ef803eed8dfb251a18d453f34a6e3ba
ryantohhr <ryantohhongrui@hotmail.com>
2025-06-16 12:55:53 +0800

    Complete first version
    Added logic to start, pause and reset stopwatch.

--- src/routes/stopwatch.tsx
+++ src/routes/stopwatch.tsx
@@ -8,13 +8,33 @@ export const Route = createFileRoute('/stopwatch')({
 function RouteComponent() {
   const [secondsElapsed, setSecondsElapsed] = useState<number>(0)
   const [stopwatchState, setStopwatchState] = useState<'start' | 'running' | 'stopped'>('start')
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | undefined>(undefined)
+
+  function intervalCallback(): void {
+    setSecondsElapsed((prev) => prev + 1)
+  }
+
+  function timeControl() {
+    if (stopwatchState === 'start' || stopwatchState === 'stopped') {
+      setIntervalId(setInterval(intervalCallback, 1000))
+      setStopwatchState('running')
+    } else if (stopwatchState === 'running') {
+      clearInterval(intervalId)
+      setStopwatchState('stopped')
+    }
+  }
+
+  function resetTime() {
+    setSecondsElapsed(0)
+    setStopwatchState('start')
+  }
 
   return (
     <div className='flex items-center justify-center h-screen'>
       {stopwatchState === 'start' && (
         <div className='w-72 h-56 flex flex-col items-center justify-evenly border border-white rounded-md'>
           {secondsElapsed} seconds elapsed
-          <button className='px-4 py-2 bg-green-500 text-white rounded-md'>
+          <button onClick={timeControl} className='px-4 py-2 bg-green-500 text-white rounded-md'>
             Start
           </button>
         </div>
@@ -22,7 +42,7 @@ function RouteComponent() {
       {stopwatchState === 'running' && (
         <div className='w-72 h-56 flex flex-col items-center justify-evenly border border-white rounded-md'>
           {secondsElapsed} seconds elapsed
-          <button className='px-4 py-2 bg-gray-500 text-white rounded-md'>
+          <button onClick={timeControl} className='px-4 py-2 bg-gray-500 text-white rounded-md'>
             Pause
           </button>
       </div>
@@ -31,10 +51,10 @@ function RouteComponent() {
         <div className='w-72 h-56 flex flex-col items-center justify-evenly border border-white rounded-md'>
           {secondsElapsed} seconds elapsed
           <div className='flex items-center justify-center gap-4'>
-            <button className='px-4 py-2 bg-gray-500 text-white rounded-md'>
+            <button onClick={timeControl} className='px-4 py-2 bg-gray-500 text-white rounded-md'>
               Resume
             </button>
-            <button className='px-4 py-2 bg-red-500 text-white rounded-md'>
+            <button onClick={resetTime} className='px-4 py-2 bg-red-500 text-white rounded-md'>
               Reset
             </button>
           </div>
