9434aee770bb1ce75925e5efc0eed3bd29f9a61b
ryantohhr <ryantohhongrui@hotmail.com>
2025-06-21 23:31:58 +0800

    Refactored final version (Gradient)
    1. Refactored single object state for gradient styles into 3 separate states
    2. Binded states to their respective input elements
    3. Improved styling
    4. Refactored onChange functions to use arrow functions inline

--- src/routes/gradient.tsx
+++ src/routes/gradient.tsx
@@ -1,58 +1,45 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { ChangeEvent, useState } from 'react'
-import { start } from 'repl'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/gradient')({
   component: RouteComponent,
 })
 
-type GradientStyle = {
-  start: string,
-  end: string,
-  direction: string
-}
-
-const startGradient = {
-  start: '#3437eb',
-  end: '#34eb4c',
-  direction: 'right'
-}
-
 function RouteComponent() {
-  const [gradientStyle, setGradientStyle] = useState<GradientStyle>(startGradient)
-
-  function handleGradientChange(e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) {
-    if (e.target.id === 'startHexInput') {
-      setGradientStyle({ ...gradientStyle, start: e.target.value })
-    } else if (e.target.id === 'endHexInput') {
-      setGradientStyle({ ...gradientStyle, end: e.target.value })
-    } else {
-      setGradientStyle({ ...gradientStyle, direction: e.target.value })
-    }
-  }
+  const [startHex, setStartHex] = useState('#eb3434')
+  const [endHex, setEndHex] = useState('#3440eb')
+  const [direction, setDirection] = useState('right')
 
   return (
-    <div className='flex items-center justify-center h-screen p-20 gap-4'>
-      <div className='flex flex-col w-1/3 h-full gap-4 border border-white rounded-md p-4'>
-        <div className='flex flex-col items-start gap-1 text-sm'>
-          From Hex
-          <input id='startHexInput' onChange={handleGradientChange} type="text" className='w-full bg-white text-black rounded-sm px-1 py-1/2'/>
-        </div>
-        <div className='flex flex-col items-start gap-1 text-sm'>
-          To Hex
-          <input id='endHexInput' onChange={handleGradientChange} type="text" className='w-full bg-white text-black rounded-sm px-1 py-1/2'/>
-        </div>
-        <div className='flex flex-col items-start gap-1 text-sm'>
-          <label htmlFor="direction">Direction</label>
-          <select id='direction' onChange={handleGradientChange} name="direction" className='w-full bg-white text-black rounded-sm'>
-            <option value="top">Top</option>
-            <option value="bottom">Bottom</option>
-            <option value="left">Left</option>
-            <option value="right">Right</option>
-          </select>
-        </div>
-      </div>
-      <div style={{background: `linear-gradient(to ${gradientStyle.direction}, ${gradientStyle.start}, ${gradientStyle.end})`}} className='w-2/3 h-full border border-white rounded-md'>
+    <div className='flex h-screen p-20 gap-4'>
+      <form className='text-sm flex flex-col gap-2 border border-white rounded-md p-4'>
+        <label htmlFor='startHex'>From Hex</label>
+        <input 
+          name='startHex' 
+          onChange={(e) => setStartHex(e.target.value)} 
+          type='text'
+          className='w-full bg-gray-300 text-black rounded-sm px-2 py-1'/>
+        <label htmlFor='endHex'>To Hex</label>
+        <input 
+          name='endHex' 
+          onChange={(e) => setEndHex(e.target.value)} 
+          type='text'
+          className='w-full bg-gray-300 text-black rounded-sm px-2 py-1'/>
+        <label htmlFor="direction">Direction</label>
+        <select 
+          name='direction' 
+          onChange={(e) => setDirection(e.target.value)} 
+          className='w-full bg-gray-300 text-black p-2 rounded-sm'>
+          <option value="top">Top</option>
+          <option value="bottom">Bottom</option>
+          <option value="left">Left</option>
+          <option value="right">Right</option>
+        </select>
+      </form>
+      <div
+        style={{
+          background: `linear-gradient(to ${direction}, ${startHex}, ${endHex})`}}
+        className='flex-grow w-2/3 h-full border border-white rounded-md'>
       </div>
     </div>
   )
